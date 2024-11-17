fe58cc76c2f3c42af0040ed5044ca99ee6dff486
trill aws <trill_dismay_0s@icloud.com>
2024-11-17 11:10:34 +0000

    cdk init + gp init (again)

---
+++ test/cicd-tut.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CicdTut from '../lib/cicd-tut-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cicd-tut-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CicdTut.CicdTutStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
